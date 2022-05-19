fe2534a4c0f7ffbc0d018841c7499b5a8d8b6d36
Aaron Robinson <41325732+ASTRobinson@users.noreply.github.com>
2022-05-19 11:46:36 +0100

    Update renovate-config.js

--- renovate/renovate-config.js
+++ renovate/renovate-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     branchPrefix: 'test-renovate/',
-    dryRun: false,
+    dryRun: true,
     username: 'renovate-release',
     gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
     onboarding: true,
